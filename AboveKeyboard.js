8eaafbb2d72f02d254a33a83c91fa231fab72fbf
stevieoj <stevenelnino101@gmail.com>
2019-03-20 13:35:23 +0100

    Initial commit

---
+++ AboveKeyboard.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import PropTypes from "prop-types";
+import { KeyboardAvoidingView, Keyboard, Animated } from 'react-native';
+
+export default class AboveKeyboard extends Component {
+  static propTypes = {
+    children: PropTypes.element.isRequired,
+    containerStyle: PropTypes.object,
+    keyboardAvoidingViewBehaviour: PropTypes.string
+  }
+
+  static defaultProps = {
+    keyboardAvoidingViewBehaviour: "padding"
+  }
+
+  constructor(props) {
+    super(props);
+    this.padding = new Animated.Value(0);
+  }
+
+  componentWillMount() {
+    this.keyboardWillShowListener = Keyboard.addListener('keyboardWillShow', this.keyboardWillShow);
+    this.keyboardWillHideListener = Keyboard.addListener('keyboardWillHide', this.keyboardWillHide);
+  }
+
+  keyboardWillShow = (event) => {
+    Animated.timing(this.padding, {
+      duration: event.duration,
+      toValue: event.endCoordinates.height,
+    }).start();
+  };
+
+  keyboardWillHide = (event) => {
+    Animated.timing(this.padding, {
+      duration: event.duration,
+      toValue: 0,
+    }).start();
+  };
+
+  render() {
+    const { containerStyle, children, keyboardAvoidingViewBehaviour } = this.props;
+    return (
+      <KeyboardAvoidingView behavior={keyboardAvoidingViewBehaviour} style={{ flex: 1 }}>
+        <Animated.View style={[{ marginBottom: this.padding, position: "absolute", bottom: 0 }, containerStyle]}>
+          {children}
+        </Animated.View>
+      </KeyboardAvoidingView>
+    );
+  }
+}
